6019c402799ca9cbfde2ad1c9d8e7ea4b1aad280
David Calle <davidcalleg@gmail.com>
2022-05-20 08:51:32 -0500

    pokemon banner implemented in home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,11 +5,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('userspace.home.index');
+        $pokemon_id = random_int(1, 700);
+        $pokemon = Http::get('https://pokeapi.co/api/v2/pokemon/' . $pokemon_id)->json();
+
+        $viewdata = [
+            'pokemon' => $pokemon
+        ];
+
+        return view('userspace.home.index')->with("viewData", $viewdata);
     }
 }
